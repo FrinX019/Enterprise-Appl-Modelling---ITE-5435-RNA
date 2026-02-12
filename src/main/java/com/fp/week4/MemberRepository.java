27c30256fafac26564424db7ce3eb62196384e8a
frinx019 <frinpatel1841974@gmail.com>
2026-02-12 14:04:28 -0500

    Week 5 Rest Api

--- src/main/java/com/fp/week4/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fp.week4;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends ReactiveMongoRepository<Member, String> {
-}
